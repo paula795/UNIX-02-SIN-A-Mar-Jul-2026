099213e3b0b6d48ff5fa328da651a4117367c534
paula795 <pasimbanape@uide.edu.ec>
2026-03-20 18:05:50 +0000

    ultimos comandos

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,5 @@ uname -a #Da informacion del ordenador
 which gpg #ubicación del binario del gpg 
 gpg --version #muestra la version e info de gpg
 gpg --full-key-generate #genero llave publica y privada
+gpg --list-keys # lista las llaves
+gpg --armor --export #Exporta las llaves
