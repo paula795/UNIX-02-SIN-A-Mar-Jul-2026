e764f8a87c392b30d0b1b09048d92d5a7d1bdb8b
paula795 <pasimbanape@uide.edu.ec>
2026-03-26 23:50:11 +0000

    corrected comments

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,7 +1,7 @@
-uname -a #It provides information about the computer.
-which gpg #location of the gpg binary 
-gpg --version #Displays the version and information of GPG
-gpg --full-key-generate #I generate public and private keys
+uname -a # Displays detailed system information including kernel, OS, and architecture.
+which gpg # Shows the full path where the GPG binary is installed.
+gpg --version # Displays the version and information of GPG.
+gpg --full-key-generate #I generate public and private keys.
 gpg --list-keys # list the keys
 gpg --armor --export #Export the keys
 gpg --list-secret-keys --keyid-format=long # to list the private keys
@@ -11,7 +11,7 @@ gpg --import Ariel.asc #To import my colleague's public key from the file
 echo "Chupiseñal" > doc_no_cifrado.text # To create the file and import the text
 gpg --output doc_cifrado.text --encrypt --recipient buitronariel23@gmail.com doc_no_cifrado.text #Encrypt the contents of our file
 gpg --decrypt doc_Arielcifrado.txt #It is used to decrypt my colleague's message.
-gpg --output doc_no_cifrado_firmado.text --clearsign doc_no_cifrado.text # A document is created
+gpg --output doc_no_cifrado_firmado.text --clearsign doc_no_cifrado.text # Creates a digitally signed version of the document in readable format, preserving the original content.
 cat doc_no_cifrado_firmado.txt # It is verified that the document is located
 gpg --verify doc_no_cifrado_firmado.txt #It is used to verify that the document is signed. 
 gpg --output doc_no_cifrado_firmado_binario.text --sign doc_cifrado.text #This command is used to sign the document in binary format.
@@ -20,7 +20,7 @@ gpg --output firma_separada_doc_no_cifrado.sig --detach-sign doc_no_cifrado.text
 gpg --verify firma_separada_doc_no_cifradoAriel.sig doc_no_cifrado.txt # It serves to verify my colleague's separate signature.
 gpg --edit-key buitronariel23@gmail.com #This command is used to give a vote of confidence
 gpg --sign-key 3AFF0D971913C2E2 # This command is used to sign and certify trust
-gpg --verify doc_no_cifrado_firmado.txt #This command is used to verify my colleague's signed document.
-gpg --output doc_cifrado_y_firmado.txt --encrypt --sign --recipient buitronariel23@gmail.com doc_no_cifrado.text #This command is used to sign and encrypt the document
-gpg --output doc_cifrado_y_firmado_descifrado_y_validado.txt --decrypt doc_cifrado_y_firmadoAriel.txt # This command is used to validate the signature and decrypt.
-cat doc_cifrado_y_firmado_descifrado_y_validado.txt # We use this command to check the contents of the file.
+gpg --verify doc_no_cifrado_firmado.txt #Verifies again that the signed document is valid and unchanged.
+gpg --output doc_cifrado_y_firmado.txt --encrypt --sign --recipient buitronariel23@gmail.com doc_no_cifrado.text #Encrypts and signs the file at the same time for confidentiality and authenticity.
+gpg --output doc_cifrado_y_firmado_descifrado_y_validado.txt --decrypt doc_cifrado_y_firmadoAriel.txt # Decrypts the file and automatically verifies the signature.
+cat doc_cifrado_y_firmado_descifrado_y_validado.txt # # Displays the final decrypted and verified content.
