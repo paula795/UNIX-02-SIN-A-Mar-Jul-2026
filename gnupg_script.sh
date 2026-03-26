93e87342c45b43554269e9ca7a809b1f05479f4c
paula795 <pasimbanape@uide.edu.ec>
2026-03-26 13:00:50 +0000

    llave ariel

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,3 +6,4 @@ gpg --list-keys # lista las llaves
 gpg --armor --export #Exporta las llaves
 gpg --list-secret-keys --keyid-format=long # para listar las llaves privadas
 gpg --armor --export-secret-keys xxxxxx # exporta la clave privada
+ gpg --armor --export paulitagaby12@gmail.com >> mi_llave_publica.asc #Para exportar mi llave publica 
