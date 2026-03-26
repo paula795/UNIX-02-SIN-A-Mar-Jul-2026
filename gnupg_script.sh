575a0516b0280631292e065706da97a2a58fba25
paula795 <pasimbanape@uide.edu.ec>
2026-03-26 13:18:13 +0000

    cifrado

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,4 +6,7 @@ gpg --list-keys # lista las llaves
 gpg --armor --export #Exporta las llaves
 gpg --list-secret-keys --keyid-format=long # para listar las llaves privadas
 gpg --armor --export-secret-keys xxxxxx # exporta la clave privada
- gpg --armor --export paulitagaby12@gmail.com >> mi_llave_publica.asc #Para exportar mi llave publica 
+gpg --armor --export paulitagaby12@gmail.com >> mi_llave_publica.asc #Para exportar mi llave publica 
+gpg --import Ariel.asc #Para importar la llave publica de mi compañero desde el archivo
+echo "Chupiseñal" > doc_no_cifrado.text # Para crear el archivo e i8mporta el texto 
+gpg --output doc_cifrado.text --encrypt --recipient buitronariel23@gmail.com doc_no_cifrado.text #Cifra el contenido de nuestro archivo
