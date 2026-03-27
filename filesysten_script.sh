d6f15f9f87a9f2af9b9d11dfa059b17efb5af6ec
paula795 <pasimbanape@uide.edu.ec>
2026-03-27 17:23:26 +0000

    pwd y ls-F

--- filesysten_script.sh
+++ filesysten_script.sh
@@ -9,11 +9,19 @@ cd/proc
 ls
 # Displays the contents of the cpuinfo file.
 cat cpuinfo
-
+#Changes directory to that specific project folder.
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/
+#Without arguments, it takes you back to your home directory.
 cd
+#Moves the file saludobin to the /bin/ directory (system binaries folder).
 sudo mv saludobin /bin/
+#Executes the program named saludobin (if it exists and has execution permissions).
 saludobin
+#Changes directory to /etc/gss, a folder related to system configuration.
 cd /etc/gss
+Shows the command history again.
 history
- pwd #Nos ayuda a ver la ruta del directorio 
+#It helps us see the directory path
+pwd 
+#It displays files and folders by adding a symbol at the end.
+ls -F
