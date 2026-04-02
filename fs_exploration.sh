98c3a606334eebb504530f1c4e385fb1ae10866f
paula795 <pasimbanape@uide.edu.ec>
2026-04-02 12:46:05 +0000

    Installing Parted on Linux and commands to install Parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,9 @@
 cd /
 # It displays the files and folders in your current location, adding symbols like / to identify which are directories.
 ls -F
+# Update the list of available packages from the repositories.
+sudo apt update
+# Update installed programs to their latest version.
+sudo apt upgrade
+# Install the parted tool on the system.
+sudo apt install parted
