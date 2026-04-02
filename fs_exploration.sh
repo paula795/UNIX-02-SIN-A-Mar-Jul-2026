c4a9defbc30977b268618d5bcb82d147a62f5809
paula795 <pasimbanape@uide.edu.ec>
2026-04-02 12:29:54 +0000

    Exploring the file system with cd and ls

---
+++ fs_exploration.sh
@@ -0,0 +1,4 @@
+# It is used to change directories and takes you to the system root, that is, the main folder where all the others are located.
+cd /
+# It displays the files and folders in your current location, adding symbols like / to identify which are directories.
+ls -F
