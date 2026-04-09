4394890e2af76706636e0b25ec1d93d7df9cf2d9
paula795 <pasimbanape@uide.edu.ec>
2026-04-09 12:33:17 +0000

    Relative path

--- script_boot.sh
+++ script_boot.sh
@@ -5,4 +5,10 @@ mkdir
 cd proyecto/ 
 # This command takes you directly to that complete route, no matter where you were before.
 #absolute path
+#Relative path
  cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
+ # It creates the path to the root directory no matter where I am.
+ cd
+ # It takes us to the home directory (~ this symbol makes us move to the home directory because it is its symbol)
+ cd ~ 
+ #
