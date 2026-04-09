2db5a3e79cc37fac0d159a9c78ebb6e7211234c9
paula795 <pasimbanape@uide.edu.ec>
2026-04-09 12:26:36 +0000

    Working with absolute and relative paths in Linux

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+# This command creates a new folder called project in the current directory.
+mkdir 
+# Here you move (change directory) to the project folder you just created.
+#relative path
+cd proyecto/ 
+# This command takes you directly to that complete route, no matter where you were before.
+#absolute path
+ cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
