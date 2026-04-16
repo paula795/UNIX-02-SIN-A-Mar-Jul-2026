3ac6ef77151ad81ac69d7a816ac11bb89d356a3a
paula795 <pasimbanape@uide.edu.ec>
2026-04-16 12:49:00 +0000

    ls commands to view detailed file system information

---
+++ boot_exploration.sh
@@ -0,0 +1,10 @@
+#They allow you to list all files and directories in the current location, including hidden files.
+#short option
+ls -a 
+#They allow you to list all files and directories in the current location, including hidden files.
+#long option
+ls --all
+# The command ls -l -a -h lists all files and directories on the system, including hidden ones. It displays detailed information such as permissions, owner, size, and modification date for each file. The `-h` option presents sizes in a more readable format, such as kilobytes or megabytes, instead of bytes.
+ls -l -a -h
+#The command ls -lah performs exactly the same function as the previous one, since it combines the -l, -a and -h options into a single abbreviated form.
+ls -lah
