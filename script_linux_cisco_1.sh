6011598ebf1ff4ca52b8f49042711c64d34c91a0
paula795 <pasimbanape@uide.edu.ec>
2026-04-29 21:46:48 +0000

    Fix: installed aptitude and explained 'aptitude moo' command behavior

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -2,4 +2,8 @@
 ls 
 # Este comando se encarga de mostrar todos los archivos que se encuentran dentro de solo ese directorio, en nuestro caso no tenemos ese directorio por eso recibimos como resultado que no esta disponible
 ls documents
-#
+# Al intentar ejecutar este comando nos salio "bash: aptitude: command not found" que basicamente nos indica que el comando aptitude no está instalado en el sistema, lo cual lo solucionamos con los siguientes comaandos:
+#aptitude moo es un comando recreativo que no cumple una función técnica, sino que es una característica oculta
+aptitude moo
+sudo apt update
+sudo apt install aptitude
