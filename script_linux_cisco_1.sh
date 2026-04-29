a7bf60f9ac7639093015f7a1fac346075eda345e
paula795 <pasimbanape@uide.edu.ec>
2026-04-29 21:41:26 +0000

    Fix: attempted to list non-existent directory 'documents'

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,5 @@
 # Este comando se encarga de mostrar en una lista los archivos que se encuntran en el directorio actual.
 ls 
+# Este comando se encarga de mostrar todos los archivos que se encuentran dentro de solo ese directorio, en nuestro caso no tenemos ese directorio por eso recibimos como resultado que no esta disponible
+ls documents
 #
