d5ac42fe93c84bcf424456fc3244d12e8141ed56
paula795 <pasimbanape@uide.edu.ec>
2026-06-04 13:48:41 +0000

    Modify array elements in Bash

--- array.sh
+++ array.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-# Sets an array
+# Set an array with three IP addresses
 IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
-# Prints all elements in the array
+
+# Delete the second IP address from the array
+unset IP_ADDRESSES[1]
+
+# Replace the first IP address with a new value
+IP_ADDRESSES[0]="192.168.1.10"
+
+# Print all elements in the array
 echo "${IP_ADDRESSES[*]}"
-# Prints only the first element in the array
+
+# Print only the first element in the array
 echo "${IP_ADDRESSES[0]}"
