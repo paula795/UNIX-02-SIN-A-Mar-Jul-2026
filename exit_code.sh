78b6f1ff449b29c47c90f64ecdff4c07bd36b1e0
paula795 <pasimbanape@uide.edu.ec>
2026-06-09 17:19:16 +0000

    Fix exit code capture and improve script output messages

--- exit_code.sh
+++ exit_code.sh
@@ -4,3 +4,8 @@ ls -l > /dev/null
 echo "The exit code of the ls command was: $?"
 lzl 2> /dev/null
 echo "The exit code of the non-existing lzl command was: $?"
+
+ls 
+echo "Exting with exit code: 223"
+echo "The exit code of the ls command was: $?"
+exit 223
