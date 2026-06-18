e870539a71cbcd34407de5fe84d79968c4b73ef3
paula795 <pasimbanape@uide.edu.ec>
2026-06-18 13:09:44 +0000

    Add log filtering and field extraction commands

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -21,3 +21,9 @@ echo "Hola1,chao1,Hola2,chao2,Hola3,chao3" >> example_csv.txt
 echo "Cami,Ash,Pau" >> example_csv.txt
 echo "a,e,i,o,u" >> example_csv.txt
 awk -F',' '{print $1}' example_csv.txt
+
+head log.txt
+awk 'NR < 10' log.txt
+grep "42.236.10.117" log.txt 
+awk '{print $7}' log.txt 
+grep "42.236.10.117" log.txt | awk '{print $7}'
