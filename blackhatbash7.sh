94216bb99e1895deb878a197f7af3a138c5a6bdb
paula795 <pasimbanape@uide.edu.ec>
2026-06-19 16:46:05 +0000

    Use sed to replace Mozilla with Godzilla in log files

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -27,3 +27,8 @@ awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt 
 awk '{print $7}' log.txt 
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/Mozilla/Godzilla/g' log.txt
+grep "Mozilla" log.txt
+grep "Godzilla" log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
